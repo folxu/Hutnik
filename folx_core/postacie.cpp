23883bfde80ae6b5d117f30c15eaef75169e354f
folxu <32227397+folxu@users.noreply.github.com>
2017-10-13 21:34:58 +0200

    update
    maly update, switch + if

--- folx_core/postacie.cpp
+++ folx_core/postacie.cpp
@@ -10,24 +10,48 @@ glownybohater::glownybohater() {} // ??????
 
 void glownybohater::wczytajwalke( poboczna Baran )
 {
+
     int wybor;
+    int codalej;
 
     cout << "Co chcesz zrobic?" << endl;
     cout << "1. Uderz przeciwnika" << endl;
     cout << "2. Wyjdz z walki" << endl;
-    cout << "Twoj wybor:" << endl;
+    cout << "Twoj wybor:";;
     cin >> wybor;
+    cout << endl;
+    cout << endl;
 
     switch (wybor)
     {
     case 1:
+        Sleep(300);
+        cout << "Przygotowywujesz si\251 do walki.." << endl;
+        Sleep(1500);
+        cout << "Wykonujesz pierwszy cios.." << endl;
+        Sleep(1000);
         BohaterZadanyDMG = BohaterAtak + BohaterSila;
-        cout << "Zadales przeciwnikowi: " << BohaterZadanyDMG << " obrazen " << endl;
+        cout << "Zadajesz przeciwnikowi: " << BohaterZadanyDMG << " obrazen " << endl;
         Baran.PPBZostaloHP = Baran.PPBHP - BohaterZadanyDMG;
+        Sleep(500);
         cout << "Przeciwnikowi zostalo: " << Baran.PPBZostaloHP << " zdrowia " << endl;
+
+        if (Baran.PPBZostaloHP<100)
+        {
+            cout << endl;
+            cout << "Co dalej?" << endl;
+            cin >> codalej;
+        }
+        else
+        {
+            cout << "Brawo! Pokona³eœ " << Baran.imie << endl;
+        }
+
         break;
     case 2:
-        cout << "Nie mozesz uciec!";
+        cout << "Skille? a na co to komu?" << endl;
+    case 3:
+        cout << "Kolegooo...nie uciekamy z walki ;)" << endl;
     default:
         cout << "Wybierz 1 lub 2!" << endl;
         break;
